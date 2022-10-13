1a97965f1d8c0eca615b23c4aa7b118c6a6cac3a
Ketan Jakhar <70067813+ketan-jakhar@users.noreply.github.com>
2022-10-14 03:05:44 +0530

    [feat]: Discover + Pause/Play functionality added

---
+++ src/pages/Discover.jsx
@@ -0,0 +1,59 @@
+import { useSelector, useDispatch } from "react-redux";
+import { Error, Loader, SongCard } from "../components";
+import { genres } from "../assets/constants";
+import { useGetTopChartsQuery } from "../redux/services/shazamCore";
+
+const Discover = () => {
+  const dispatch = useDispatch();
+  const { activeSong, isPlaying } = useSelector((state) => state.player);
+  const { data, isFetching, error } = useGetTopChartsQuery();
+  const genreTitle = "Pop";
+
+  console.log(data);
+
+  if (isFetching) return <Loader title="Loading songs..." />;
+
+  if (error) return <Error />;
+  return (
+    <div className="flex flex-col">
+      <div className="w-full flex justify-between items-center sm:flex-row flex-col mt-4 mb-10">
+        <h2 className="font-bold text-3xl text-white text-left">
+          Discover {genreTitle}
+        </h2>
+        <select
+          onChange={() => {}}
+          value=""
+          className="bg-black text-gray-300 p-3 text-sm rounded-lg outline-none sm:mt-0 focus:ring-2 focus:ring-gray-600 focus:ring-opacity-50 active:ring-2 active:ring-gray-600 active:ring-opacity-50"
+        >
+          {genres.map((genre) => {
+            return (
+              <option
+                key={genre.value}
+                value={genre.value}
+                className="border-none outline-none"
+              >
+                {genre.title}
+              </option>
+            );
+          })}
+        </select>
+      </div>
+      <div className="flex flex-wrap sm:justify-start justify-center gap-8">
+        {data?.map((song, i) => {
+          return (
+            <SongCard
+              key={song.key}
+              song={song}
+              i={i}
+              isPlaying={isPlaying}
+              isActiveSong={activeSong}
+              data={data}
+            />
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default Discover;
---
+++ src/redux/services/shazamCore.js
@@ -0,0 +1,21 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+export const shazamCoreApi = createApi({
+  reducerPath: "shazamCoreApi",
+  baseQuery: fetchBaseQuery({
+    baseUrl: "https://shazam-core.p.rapidapi.com/v1",
+    prepareHeaders: (headers) => {
+      headers.set(
+        "X-RapidAPI-Key",
+        "e2f0d6d4c8msh49198382a6075f2p1f5ce4jsn65bc987bd28e"
+      );
+      headers.set("X-RapidAPI-Host", "shazam-core.p.rapidapi.com");
+      return headers;
+    },
+  }),
+  endpoints: (builder) => ({
+    getTopCharts: builder.query({ query: () => "/charts/world" }),
+  }),
+});
+
+export const { useGetTopChartsQuery } = shazamCoreApi;
